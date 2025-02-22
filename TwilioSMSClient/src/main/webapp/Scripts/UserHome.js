85f69b26e8cf401e95ee564aa03c2f2a5195158d
mahmoud ibrahim <mibrahim@localhost.localdomain>
2025-02-22 02:30:47 +0200

    new updated

--- TwilioSMSClient/src/main/webapp/Scripts/UserHome.js
+++ TwilioSMSClient/src/main/webapp/Scripts/UserHome.js
@@ -1,12 +1,13 @@
-        document.addEventListener("DOMContentLoaded", function () {
-            fetch("/TwilioSMSClient/UserSessionServlet")
-                .then(response => response.json())
-                .then(data => {
-                    if (data.username) {
-                        document.getElementById("usernameDisplay").textContent = data.username;
-                    } else {
-                        window.location.href = "login.html"; // Redirect if not logged in
-                    }
-                })
-                .catch(error => console.error("Error fetching username:", error));
-        });
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("/TwilioSMSClient/UserSessionServlet")
+            .then(response => response.json())
+            .then(data => {
+                if (data.username) {
+                    document.getElementById("usernameDisplay").textContent =
+                            data.username.charAt(0).toUpperCase() + data.username.slice(1);
+                } else {
+                    window.location.href = "login.html"; // Redirect if not logged in
+                }
+            })
+            .catch(error => console.error("Error fetching username:", error));
+});
